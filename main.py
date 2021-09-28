409619ef293652373192177ccf69989e5213982e
Marilsa-Barros <marilsa_barros@hotmail.com>
2021-09-27 22:11:48 -0300

    Uso do paratrize do PyTest Uso do Try-Except

--- main.py
+++ main.py
@@ -20,6 +20,23 @@ def dividir(numero1, numero2):
         return numero1 / numero2
     else:
         return 'Não dividirás por zero'
+
+def dividir_try_except(numero1, numero2):
+    try:
+        return numero1 / numero2
+    except TypeError:
+        #return 'Não dividirás por zero'
+        if TypeError == ZeroDivisionError:
+            return 'Não dividirás por zero'
+        elif TypeError == ArithmeticError:
+            return 'Erro no cálculo'
+        elif TypeError == ValueError:
+            return 'Erro no valor'
+        else:
+            return 'Erro desconhecido'
+        pass
+
+
  # Teste Unitários / Teste de Unidades
 
 # teste da função somar
@@ -42,11 +59,15 @@ def test_somar_didatico():
 #valores
     (5, 4, 9), # teste 1
     (3, 2, 5), # teste 2
-    (10,6, 16)  # teste 3
+    (10,6, 15)  # teste 3
 ])
 
 def test_somar(numero1, numero2, resultado):
-    assert somar(numero1,numero2) == resultado
+    try:
+        assert somar(numero1,numero2) == resultado
+    except AssertionError:
+        print(f'Entrou no Except: {AssertionError}')
+        pass
 
 def test_somar_resultado_negativo():
     assert somar(-1000, -2000) == -3000
@@ -57,6 +78,24 @@ def test_subtrair():
 def test_multiplicar():
     assert multiplicar(3,7) == 21
 
+def test_dividir():
+        assert dividir(8, 4) == 2
+
+
+def test_dividir_por_zero():
+    assert dividir(8, 0) == 'Não dividirás por zero'
+@pytest.mark.parametrize('numero1, numero2, resultado', [
+    (8,2,4),
+    (20,4,5),
+    (10,0, 'Não dividirás por zero')
+])
+def test_dividir_try_expect(numero1, numero2, resultado):
+    assert dividir_try_except(numero1,numero2) == resultado
+
+
+    # teste positivo --> mostrar o resultado correto
+    # teste positivo --> avançar para próxima tela
+    # teste negativo --> mostrar mensagem de erro
 
 if __name__ == '__main__':
     print_hi('Marilsa')
@@ -74,7 +113,7 @@ if __name__ == '__main__':
     print(f'O resultado da multiplicação: {resultado}')
 
     # divisão
-    resultado = dividir(9,3)
+    resultado = dividir(8,0)
     print(f'O resultado da divisão: {resultado}')
 
 # Dia 1 : 100 testes : 0 passaram
